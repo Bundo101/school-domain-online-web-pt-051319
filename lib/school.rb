4c46fdc8bd5eafcaf85d5214e83791e946216061
James Britton <brittonjp@gmail.com>
2019-07-14 12:06:45 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,23 +1,28 @@
+require 'pry'
+
 class School
   
-  attr_accessor :name, :roster
+  attr_accessor :roster
   
   def initialize(name)
     @name = name
     @roster = {}
   end
   
-  def add_student(student_name, grade)
+  def add_student(name, grade)
     @roster[grade] ||= []
-    @roster[grade] << student_name
+    @roster[grade] << name
   end
   
   def grade(grade)
-    @roster[grade] if @roster.has_key?(grade)
+    @roster[grade]
   end
   
   def sort
-    @roster.each { |grade, students| students.sort! }
+    self.roster.each do |grade, students|
+      students.sort!
+      #binding.pry
+    end
   end
   
 end
